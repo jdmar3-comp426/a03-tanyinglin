0e48d9c5bfbf7ea8eacc9b24f91d5d69d973848a
Tanying <tlin74832@gmail.com>
2021-11-13 16:44:12 -0500

    fix medium_1

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -26,7 +26,9 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-    array.sort();
+    array.sort(function(a,b){
+        return a-b;
+      });
     const mid = Math.floor(array.length / 2);
     if ((array.length % 2) === 0){
         return (array[mid] + array[mid-1]) / 2;
