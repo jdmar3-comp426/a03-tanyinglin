df8b084f5883f128b7ecb8dbb29c68e0d383013c
Tanying <tlin74832@gmail.com>
2021-11-19 13:43:29 -0500

    spicy with return

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -174,7 +174,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-    everyEven(arr, x => x % 2 ==1)
+    return everyEven(arr, x => x % 2 ==1)
 };
 
 
@@ -184,7 +184,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-    someEven(arr, x => x % 2 ==1)
+    return someEven(arr, x => x % 2 ==1)
 };
 
 
@@ -195,7 +195,7 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-    res = filter(arr, test);
+    var res = filter(arr, test);
     if(res["pass"].length === n){
         return true;
     }
