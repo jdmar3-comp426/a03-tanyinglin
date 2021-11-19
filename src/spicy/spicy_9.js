8488b99175add71b88ea2cf59257b694b9d45663
Tanying <tlin74832@gmail.com>
2021-11-19 11:13:22 -0500

    full test

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -151,7 +151,20 @@ export const someEven = (arr, test) => {
  *       -->  { pass: [1, 5, 31], fail: [90] }
  */
 export const filter = (arr, test) => {
-
+    res = {};
+    failed = [];
+    passed = [];
+    for (let i = 0; i < arr.length; i++){
+        if(test(arr[i])){
+            passed.push(arr[i]);
+        }
+        else {
+            failed.push(arr[i]);
+        }
+    }
+    res["fail"] = failed;
+    res["pass"] = passed;
+    return res;
 };
 
 
@@ -161,7 +174,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    everyEven(arr, x => x % 2 ==1)
 };
 
 
@@ -171,7 +184,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    someEven(arr, x => x % 2 ==1)
 };
 
 
@@ -182,5 +195,9 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    res = filter(arr, test);
+    if(res["pass"].length === n){
+        return true;
+    }
+    return false;
 };
